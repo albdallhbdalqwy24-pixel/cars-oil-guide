73b0f412979a3b781718c5422259086a8383e2ce
Boqased Al-Yafai <albdallhbdalqwy24@gmail.com>
2026-08-28 14:20:10 +0300

    fix root vite config base path and build

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  base: '/cars-oil-guide/',
   root: "client",
   base: "/cars-oil-guide/",
   plugins: [react()],
