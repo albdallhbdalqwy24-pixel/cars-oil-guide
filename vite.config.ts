79a8dd1296d325484db4b069087d50f401ddedc6
Boqased Al-Yafai <albdallhbdalqwy24@gmail.com>
2026-08-28 13:54:26 +0300

    fix github pages config and add 404

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   root: "client",
+  base: "/cars-oil-guide/",
   plugins: [react()],
   server: { allowedHosts: true },
   build: { outDir: "../dist", emptyOutDir: true },
